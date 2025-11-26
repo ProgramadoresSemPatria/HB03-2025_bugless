ac1eebb3e33d4bcbd0444202532fcc65d657536b
alexsousadev <alexandrosousapro@gmail.com>
2025-11-26 11:44:55 -0300

    feat: Add initial server project setup

---
+++ back/server.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+import envLoader from "./src/services/env-loader.service";
+
+const PORT = envLoader.getEnv("PORT")
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("API is running");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on http://localhost:" + PORT);
+});
