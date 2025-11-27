b69d17e5626a1e06fcd73101e5a31193ee87b96c
yuribodo <yemotalara@gmail.com>
2025-11-27 00:19:59 -0300

    feat: add services (git, config, api)

---
+++ cli/src/services/index.ts
@@ -0,0 +1,4 @@
+export { gitService, GitService } from './git.js';
+export { configService } from './config.js';
+export { createReviewAPI, type ReviewAPI } from './api.js';
+export { MockReviewAPI, mockReviewAPI } from './mock-api.js';
