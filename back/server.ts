625946eb25edfca28bc082f168a92709550c96ba
alexsousadev <alexandrosousapro@gmail.com>
2025-11-27 16:20:06 -0300

    feat: implement user registration flow

--- back/server.ts
+++ back/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import statusSubmissionRouter from "./src/routes/status-submission.routes";
 import envLoader from "./src/services/env-loader.service";
+import authRouter from "./src/routes/auth.routes";
 
 const PORT = envLoader.getEnv("PORT")
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/status-submissions", statusSubmissionRouter);
+app.use("/auth", authRouter);
 
 app.get("/", async (req: Request, res: Response) => {
   res.send("API is running");
---
+++ back/src/services/user.service.ts
@@ -0,0 +1,90 @@
+import prisma from "../database/prisma";
+import { UserSchema, GetUserByIdSchema, GetUserByEmailSchema } from "../schemas/user.schema";
+import { hashSync } from "bcrypt";
+
+class UserService {
+
+    async createUser(data: UserSchema){
+        try {
+            const hashSalt = 10
+            const hashedPassword = hashSync(data.password, hashSalt)
+
+            const user = await prisma.user.create({
+                data: {
+                    name: data.name,
+                    email: data.email,
+                    password: hashedPassword
+                }
+            })
+
+            if(!user){
+                return null
+            }
+
+            return user
+        } catch (error) {
+            console.error("Error in createUser service:", error);
+            throw error;
+        }
+    }
+
+    async getUserByEmail(data: GetUserByEmailSchema){
+        const user = await prisma.user.findUnique({
+            where: { email: data.email }
+        })
+
+        if(!user){
+            return null
+        }
+
+        return user
+    }
+
+    async updateUser(data: GetUserByIdSchema, updateData: UserSchema){
+        const hashSalt = 10
+        const hashedPassword = hashSync(updateData.password, hashSalt)
+
+        const user = await prisma.user.update({
+            where: { id: data.id },
+            data: { 
+                name: updateData.name, 
+                email: updateData.email, 
+                password: hashedPassword 
+            }
+        })
+
+        if(!user){
+            return null
+        }
+
+        return user
+    }
+    
+    async deleteUser(data: GetUserByIdSchema){
+        const user = await prisma.user.delete({
+            where: { id: data.id }
+        })
+
+        if(!user){
+            return null
+        }
+
+        return user
+    }
+
+    async checkIfUserExistsByEmail(data: UserSchema){
+        const user = await prisma.user.findFirst({
+            where: { email: data.email }
+        })
+
+        if (!user) {
+            return false;
+        }
+
+        return true;
+    }
+}
+
+const userService = new UserService();
+
+export default userService;
